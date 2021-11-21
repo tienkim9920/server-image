bf4be96732ee7326386b3825e6f9f01981080e43
tienkim9920 <tienkim9920@gmail.com>
2021-11-21 12:18:20 +0700

    update

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 8000;
+
+const cors = require("cors");
+var upload = require('express-fileupload');
+
+app.use(cors());
+
+app.use('/', express.static('public'))
+app.use(upload());
+
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/image', async (req, res) => {
+
+    var fileImage = req.files.file;
+
+    var fileName = fileImage.name
+
+    // create path to client get image
+    // var fileProduct = "https://server-lover.herokuapp.com/" + fileName
+    var fileProduct = "http://localhost:8000/" + fileName
+
+    // move file name in folder public
+    fileImage.mv('./public/' + fileName)
+
+    res.json(fileProduct)
+})
+  
+server.listen(PORT, () => {
+    console.log('listening on *: ' + PORT);
+});
