47dda610b8d9e2b841097b91c29761ba061de638
tienkim9920 <tienkim9920@gmail.com>
2022-05-06 15:00:55 +0700

    update

--- index.js
+++ index.js
@@ -13,6 +13,28 @@ var upload = require('express-fileupload');
 app.use('/', express.static('public'))
 app.use(upload());
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Using API Blogs',
+            version: '1.0.0',
+        },
+        servers: [
+            {
+                url: 'http://localhost:8000/'
+            }
+        ]
+    },
+    apis: ['./routes/*.js'], 
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,76 +43,9 @@ app.use(bodyParser.json());
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://tienkim9920:U4tQMg6Wfy8DaL@cluster0.rco6f.mongodb.net/Blog?retryWrites=true&w=majority")
 
-const Blog = require("./model/blog.model");
+const blogsAPI = require('./routes/blogs.routes');
 
-app.get('/blogs', async (req, res) => {
-    const blogs = await Blog.find();
-    res.json(blogs)
-})
-
-app.post('/blogs', async (req, res) => {
-    try {
-        const blog = await Blog.create(req.body);
-        res.json(blog._id);
-    } catch (error) {
-        res.json(error);
-    }
-})
-
-app.patch('/blogs', async (req, res) => {
-    try {
-        const { _id, title, username, body, phone } = req.body;
-        let blog = await Blog.findById({ _id });
-        if (title) {
-            blog.title = title;
-        }
-        if (username) {
-            blog.username = username;
-        }
-        if (body) {
-            blog.body = body;
-        }
-        if (phone) {
-            blog.phone = phone;
-        }
-        blog.save();
-        res.json("Update Successfully");
-    } catch (error) {
-        res.json(error);
-    }
-})
-
-app.delete('/blogs/:id', async (req, res) => {
-    try {
-        await Blog.deleteOne({ _id: req.params.id });
-        res.json("Delete Successfully");
-    } catch (error) {
-        res.json(error);
-    }
-})
-
-app.get('/blogs/:id', async (req, res) => {
-    try {
-        const blog = await Blog.findById({ _id: req.params.id });
-        res.json(blog);
-    } catch (error) {
-        res.json(error);
-    }
-})
-
-app.post('/image', async (req, res) => {
-    var fileImage = req.files.file;
-    var fileName = fileImage.name
-    try {
-        // create path to client get image
-        var fileProduct = "https://tienkim-crud.herokuapp.com/" + fileName
-        // move file name in folder public
-        fileImage.mv('./public/' + fileName)
-        res.json(fileProduct)
-    } catch (error) {
-        res.json(error)
-    }
-})
+app.use('/blogs', blogsAPI);
 
 server.listen(PORT, () => {
     console.log('listening on *: ' + PORT);
